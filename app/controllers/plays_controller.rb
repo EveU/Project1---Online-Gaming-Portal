1a44a98607eca507c4c42b4fa2241821067272f0
Evelyn Utterson <e.utterson@hotmail.co.uk>
2016-01-12 14:18:27 +0000

    added prompt for player's turn

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,17 +1,27 @@
 class PlaysController < ApplicationController
 
   def index
+    @plays = Play.all
+  end
+
+  def new
+    @play = Play.new
+  end
+
+  def create
+
   end
 
   def show
     @play = Play.find(params[:id])
     @board = @play.display_board
+    @symbol = @play.current_piece
   end
 
   def make_move
     # raise
     play = Play.find(params[:play_id])
-    symbol = play.next_piece
+    symbol = play.current_piece
     play.moves.create(symbol: symbol, board_index: params[:board_index])
     redirect_to play_path(params[:play_id])
   end
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,7 +1,9 @@
 class Play < ActiveRecord::Base
   has_many :moves
   
-  def next_piece
+  WINNING_LINES = [ [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8] ]
+
+  def current_piece
     number_of_moves = self.moves.count
     if number_of_moves.even?
       "X"
@@ -16,6 +18,34 @@ class Play < ActiveRecord::Base
       board[move.board_index] = move.symbol
     end
     board
+  end
+
+  def check_for_win
+    status = "in progress"
+    if has_won?(current_piece)
+      "won"
+    elsif board_full?
+      "draw"
+    else
+    end
+    status
+  end
+
+  def has_won?(symbol)
+    load_board
+    WINNING_LINES.each do |wl|
+      wl.all? { || }
+    end
+
+    # def check_lines
+    #   if WINS.any? { |line| line.all? { |square| @squares[square] == @player } }
+    #     @win = @player
+    #   end
+    # end
+  end
+
+  def board_full?(symbol)
+    load_board.all?
   end
 
 end
